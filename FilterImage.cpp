96161180f7de0aaad25d3eb53a1618f23c13c68f
Maciej Adamski <madamskip1@gmail.com>
2020-10-30 21:06:01 +0100

    fix FilterImage encapsulation

--- FilterImage.cpp
+++ FilterImage.cpp
@@ -79,6 +79,11 @@ void FilterImage::filter()
 	}
 }
 
+cv::Mat& FilterImage::getResult()
+{
+	return result;
+}
+
 bool FilterImage::checkIfPointIsInside(int x, int y, int width, int height)
 {
 	return (x >= 0 && x < width&& y >= 0 && y < height);
--- FilterImage.h
+++ FilterImage.h
@@ -3,18 +3,22 @@
 
 class FilterImage
 {
-public:
-	FilterImage(const cv::Mat& source);
-	FilterImage(const cv::Mat& source, const cv::Mat& kernel);
-	void filter();
-	bool checkIfPointIsInside(int x, int y, int width, int height);
+private:
 	cv::Mat src;
 	cv::Mat result;
 	cv::Mat kernel;
 
+public:
+	FilterImage(const cv::Mat& source);
+	FilterImage(const cv::Mat& source, const cv::Mat& kernel);
 	void setKernel(const cv::Mat& newKernel);
+	void filter();
+	cv::Mat& getResult();
+
+private:
 	template<typename T>
 	void trimValue(T& value, const T& min, const T& max);
 	void setNewValues(uchar* row, int col, float val1, float val2, float val3);
+	bool checkIfPointIsInside(int x, int y, int width, int height);
 };
 
